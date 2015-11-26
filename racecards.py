c9b5cf49e67ef0cfaca154e6fe49e42df76fa62d
s89227 <helenhelens89227@gmail.com>
2015-11-26 17:24:18 +0800

    mid lane

--- racecards.py
+++ racecards.py
@@ -2,24 +2,34 @@
 import random
 
 team = []
+mid_lane = []
 
 #read teams' name
-f = open('Contestants.txt','r')
+f = open('Contestants_Team.txt','r')
 for name in f.readlines():
 	team.append(name)
 f.close()
 
-#before shuffle
-print('參賽隊伍:')
-for i in range(len(team)):
-	print(i)
-	print(team[i])
+#read mids' name
+f = open('Contestants_MidLane.txt','r')
+for name in f.readlines():
+	mid_lane.append(name)
+f.close()
 
-print('---------------------------')
 
 #suffle
-print('抽籤結果:')
 random.shuffle(team)
-for i in range(1,len(team)):
-	print(i)
+random.shuffle(mid_lane)
+
+#print result
+print('抽籤結果:')
+print('團隊賽:')
+for i in range(0,len(team)):
+	print(i+1)
 	print(team[i])
+
+print('--------------------------------------')
+print('中路單挑賽:')
+for i in range(0,len(mid_lane)):
+	print(i+1)
+	print(mid_lane[i])
