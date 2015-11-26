7edbb62c7cd3ba79346461cdb3dba5ebeee2cd42
s89227 <helenhelens89227@gmail.com>
2015-11-26 18:42:56 +0800

    done

--- racecards.py
+++ racecards.py
@@ -24,27 +24,14 @@ f.close()
 random.shuffle(team)
 random.shuffle(mid_lane)
 
-#print result
-print('抽籤結果:')
-print('團隊賽:')
-for i in range(0,len(team)):
-	print(i+1)
-	print(team[i])
-
-print('--------------------------------------')
-print('中路單挑賽:')
-for i in range(0,len(mid_lane)):
-	print(i+1)
-	print(mid_lane[i])
-
 
 #write result to file
 f = open('Result_Team.txt','a')
 for i in range(len(team)):
-	f.write(team[i])
+	f.write('%d. %s' % (i+1, team[i]))
 f.close()
 
 f = open('Result_MidLane.txt','a')
 for i in range(len(mid_lane)):
-	f.write(mid_lane[i])
+	f.write('%d. %s' % (i+1, mid_lane[i]))
 f.close()
