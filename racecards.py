4ef02cb5c6e2339e1cede71b86f488f71baa688f
s89227 <helenhelens89227@gmail.com>
2015-12-06 22:49:25 +0800

    update racecards

--- racecards.py
+++ racecards.py
@@ -4,14 +4,14 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-team = []
+"""team = []"""
 mid_lane = []
 
-#read teams' name
+"""#read teams' name
 f = open('Contestants_Team.txt','r')
 for name in f.readlines():
 	team.append(name)
-f.close()
+f.close()"""
 
 #read mids' name
 f = open('Contestants_MidLane.txt','r')
@@ -21,17 +21,22 @@ f.close()
 
 
 #suffle
-random.shuffle(team)
+"""random.shuffle(team)"""
 random.shuffle(mid_lane)
 
 
 #write result to file
-f = open('Result_Team.txt','a')
+"""f = open('Result_Team.txt','a')
 for i in range(len(team)):
 	f.write('%d. %s' % (i+1, team[i]))
-f.close()
+f.close()"""
 
 f = open('Result_MidLane.txt','a')
+for i in range(len(mid_lane)):
+	f.write('%d. %s' % (i+1, mid_lane[i]))
+f.close()
+
+f = open('Time_MidLane.txt','a')
 for i in range(len(mid_lane)):
 	f.write('%d. %s' % (i+1, mid_lane[i]))
 f.close()
